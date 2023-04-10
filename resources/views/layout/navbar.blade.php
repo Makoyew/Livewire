c2bea26775a21e59e5eba36d03043eadf06b0eaa
unknown <marklistermueblas3@gmaiil.com>
2023-04-11 00:25:22 +0800

    first commit

---
+++ resources/views/layout/navbar.blade.php
@@ -0,0 +1,15 @@
+<header class="bg-dark">
+    <div class="container-fluid">
+      <nav class="navbar navbar-expand-lg">
+        <a class="navbar-brand font-weight-bold text-light" href="/">Music Bar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMenu" aria-controls="navbarMenu" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarMenu">
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <!-- Add your navbar items here -->
+          </ul>
+        </div>
+      </nav>
+    </div>
+  </header>
